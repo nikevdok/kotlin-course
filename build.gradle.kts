5808e2bc52e988657c8c2a663779a1c0125c1375
com.nikitaevdokimov <nikitaevdokimov@live.com>
2024-12-21 15:31:51 +0200

    Commit new

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,11 +11,15 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("com.google.code.gson:gson:2.8.9")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
 }
 
 tasks.test {
     useJUnitPlatform()
-};
-{
-    implementation("com.google.code.gson:gson:2.8.9")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
